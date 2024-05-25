24f6959cf0d65434f694dfc47f0d64fcaddf4467
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-05-25 14:54:00 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -32,7 +32,7 @@ const HomeContainer = styled.div`
     left: 0;
     right: 0;
     bottom: 0;
-    background: url('/src/assets/homebg.webp') no-repeat center center fixed;
+    background: url('https://sanjaybasket.s3.ap-south-1.amazonaws.com/HogwartsEdX/homebg.webp') no-repeat center center fixed;
     background-size: cover;
     filter: blur(5px);
     z-index: -1;
