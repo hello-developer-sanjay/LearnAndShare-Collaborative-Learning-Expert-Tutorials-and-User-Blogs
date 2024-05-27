839ad08fe29865d8870df51e63476f4225895492
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-05-28 01:43:40 +0530

    Update authActions.jsx

--- frontend/src/actions/authActions.jsx
+++ frontend/src/actions/authActions.jsx
@@ -8,7 +8,7 @@ export const loadUser = () => async dispatch => {
         dispatch({ type: AUTHENTICATE_USER, payload: true });
     }
     try {
-        const res = await axios.get('http://localhost:5000/api/auth/user');
+        const res = await axios.get('https://hogwartsedx-backend-api-25may.onrender.com/api/auth/user');
         localStorage.setItem('user', JSON.stringify(res.data.user));
         dispatch({ type: FETCH_USER_SUCCESS, payload: { user: res.data.user, token: localStorage.token } });
         dispatch({ type: FOLLOW_CATEGORY_SUCCESS, payload: res.data.user.followedCategories });
@@ -29,7 +29,7 @@ export const login = (email, password) => async dispatch => {
     const body = JSON.stringify({ email, password });
 
     try {
-        const res = await axios.post('http://localhost:5000/api/auth/login', body, config);
+        const res = await axios.post('https://hogwartsedx-backend-api-25may.onrender.com/api/auth/login', body, config);
         dispatch({ type: LOGIN_SUCCESS, payload: { user: res.data.user, token: res.data.token } });
         setAuthToken(res.data.token);
         localStorage.setItem('token', res.data.token);
@@ -51,7 +51,7 @@ export const register = formData => async dispatch => {
     };
 
     try {
-        const res = await axios.post('http://localhost:5000/api/auth/register', formData, config);
+        const res = await axios.post('https://hogwartsedx-backend-api-25may.onrender.com/api/auth/register', formData, config);
         dispatch({ type: LOGIN_SUCCESS, payload: { user: res.data.user, token: res.data.token } });
         setAuthToken(res.data.token);
         localStorage.setItem('token', res.data.token);
