172aa65b32f1cd70905a8819a9daaa82c7ab126d
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-05-27 20:55:18 +0530

    Update authReducer.js

--- frontend/src/reducers/authReducer.js
+++ frontend/src/reducers/authReducer.js
@@ -1,10 +1,11 @@
-import { FETCH_USER_SUCCESS, LOGIN_SUCCESS,AUTHENTICATE_USER  } from '../actions/types';
+import { FETCH_USER_SUCCESS, LOGIN_SUCCESS,AUTHENTICATE_USER,FOLLOW_CATEGORY_SUCCESS, UNFOLLOW_CATEGORY_SUCCESS  } from '../actions/types';
 
 const initialState = {
     token: localStorage.getItem('token'),
     isAuthenticated: localStorage.getItem('token') ? true : false, // Initialize from localStorage
     loading: true,
-    user: null
+    user:null,
+    followedCategories: JSON.parse(localStorage.getItem('user'))?.followedCategories || []
 };
 
 const authReducer = (state = initialState, action) => {
@@ -21,13 +22,27 @@ const authReducer = (state = initialState, action) => {
                 token: payload.token || localStorage.getItem('token'),
                 isAuthenticated: true,
                 loading: false,
-                user: payload.user 
+                user: payload.user ,
+                followedCategories: payload.user.followedCategories
+
             };
             case AUTHENTICATE_USER:
             return {
                 ...state,
                 isAuthenticated: payload
             };
+            case FOLLOW_CATEGORY_SUCCESS:
+                console.log('Category followed:', payload);
+                return {
+                    ...state,
+                    followedCategories: [...state.followedCategories, payload]
+                };
+            case UNFOLLOW_CATEGORY_SUCCESS:
+                console.log('Category unfollowed:', payload);
+                return {
+                    ...state,
+                    followedCategories: state.followedCategories.filter(category => category !== payload)
+                };
         default:    
             console.log('No action taken:', action); // Add console log for default case
             return state; // Add default case to return the current state
