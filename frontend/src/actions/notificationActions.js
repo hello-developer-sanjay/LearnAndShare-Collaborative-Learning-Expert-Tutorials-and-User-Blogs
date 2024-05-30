0635f3f036e3e9d2be1dbc9905484ca46845cad7
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-05-30 20:22:20 +0530

    Update notificationActions.js

--- frontend/src/actions/notificationActions.js
+++ frontend/src/actions/notificationActions.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import {
     FETCH_NOTIFICATIONS_SUCCESS,
     ADD_NOTIFICATION_SUCCESS,
@@ -13,30 +16,24 @@ const updateLocalStorage = (followedCategories) => {
         const serializedState = JSON.stringify(followedCategories);
         localStorage.setItem('followedCategories', serializedState);
     } catch (err) {
-        console.error('Could not save state', err);
     }
 };
 // Fetch notifications
 export const fetchNotifications = () => async dispatch => {
     const token = localStorage.getItem("token");
     if (!token) {
-        console.error('No token found in localStorage');
         return;
     }
     setAuthToken(token);
 
-    console.log('Fetching notifications...');
-    console.log('token is : ', token);
     try {
         const response = await axios.get('https://hogwartsedx-backend-29may.onrender.com/api/categories/notifications', {
             headers: {
                 'x-auth-token': token
             }
         });
-        console.log('Notifications fetched successfully:', response.data);
         dispatch({ type: FETCH_NOTIFICATIONS_SUCCESS, payload: response.data });
     } catch (error) {
-        console.error('Error fetching notifications:', error);
     }
 };
 
@@ -45,10 +42,8 @@ export const addNotification = (notification) => async dispatch => {
     console.log('Adding notification:', notification);
     try {
         const response = await axios.post('https://hogwartsedx-backend-29may.onrender.com/api/categories/notifications', notification);
-        console.log('Notification added successfully:', response.data);
         dispatch({ type: ADD_NOTIFICATION_SUCCESS, payload: response.data });
     } catch (error) {
-        console.error('Error adding notification:', error);
     }
 };
 // Mark a notification as read
@@ -56,12 +51,9 @@ export const markNotificationAsRead = (id, token) => async dispatch => {
 
     const token = localStorage.getItem('token');
         if (!token) {
-            console.error('No token found in localStorage');
             return;
         }
         setAuthToken(token);
-    console.log(" is token available : ", token);
-    console.log(`Marking notification as read: ${id}`);
     try {
         
 
@@ -74,14 +66,29 @@ export const markNotificationAsRead = (id, token) => async dispatch => {
                 }
             }
         );
-        console.log('Notification marked as read successfully:', response.data);
         dispatch({ type: MARK_NOTIFICATION_AS_READ_SUCCESS, payload: response.data });
+        toast.success('Notification marked as read successfully ', {
+            position: 'top-right',
+            autoClose: 2000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
     } catch (error) {
-        console.error(`Error marking notification as read (${id}):`, error);
+        toast.success('Error marking as read , Please Login and try again ', {
+            position: 'top-right',
+            autoClose: 2000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
     }
 };
 export const followCategory = (category, token) => async dispatch => {
-    console.log(`Following category: ${category}`);
     try {
         const response = await axios.post(
             'https://hogwartsedx-backend-29may.onrender.com/api/categories/follow-category',
@@ -92,17 +99,32 @@ export const followCategory = (category, token) => async dispatch => {
                 }
             }
         );
-        console.log(`Category followed successfully: ${category}`, response.data);
         const followedCategories = response.data;  // Ensure this returns the updated list
         updateLocalStorage(followedCategories);
         dispatch({ type: FOLLOW_CATEGORY_SUCCESS, payload: followedCategories });
+        toast.success('You have successfully followed the category! Now you will receive timely notifications about updates in this category directly to your registered email.', {
+            position: 'top-right',
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
     } catch (error) {
-        console.error(`Error following category (${category}):`, error);
+        toast.error('Error following category. Please Login and try again.', {
+            position: 'top-right',
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });  
     }
 };
 
 export const unfollowCategory = (category, token) => async dispatch => {
-    console.log(`Unfollowing category: ${category}`);
     try {
         const response = await axios.post(
             'https://hogwartsedx-backend-29may.onrender.com/api/categories/unfollow-category',
@@ -113,32 +135,44 @@ export const unfollowCategory = (category, token) => async dispatch => {
                 }
             }
         );
-        console.log(`Category unfollowed successfully: ${category}`, response.data);
         const followedCategories = response.data;  // Ensure this returns the updated list
         updateLocalStorage(followedCategories);
         dispatch({ type: UNFOLLOW_CATEGORY_SUCCESS, payload: followedCategories });
+        toast.success('Category unfollowed successfully ', {
+            position: 'top-right',
+            autoClose: 2000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
     } catch (error) {
-        console.error(`Error unfollowing category (${category}):`, error);
+            toast.error('Error unfollowing category . Please Login and try again.', {
+            position: 'top-right',
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });  
     }
 };
 export const fetchFollowedCategories = () => async (dispatch) => {
     const token = localStorage.getItem('token');
     if (!token) {
-        console.error('No token found in localStorage');
         return;
     }
     setAuthToken(token);
 
-    console.log('Fetching followed categories...');
     try {
         const response = await axios.get('https://hogwartsedx-backend-29may.onrender.com/api/categories/followed', {
             headers: {
                 'x-auth-token': token,
             },
         });
-        console.log('Followed categories fetched successfully:', response.data);
         dispatch({ type: FETCH_FOLLOWED_CATEGORIES_SUCCESS, payload: response.data });
     } catch (error) {
-        console.error('Error fetching followed categories:', error);
     }
 };
