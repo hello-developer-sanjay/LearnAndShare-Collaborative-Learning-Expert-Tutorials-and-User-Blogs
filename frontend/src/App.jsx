01ae9240257c77bb6187ade1a40208252475d5ca
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-06-02 00:18:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
-import React , {useEffect}from 'react';
-
-import { BrowserRouter as Router, Routes, Route,useLocation } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import store from './store';
 import Layout from './components/Layout'; 
 import Home from './pages/Home';
@@ -10,7 +9,6 @@ import Dashboard from './pages/Dashboard';
 import AdminDashboard from './components/AdminDashboard';
 import PostPage from './components/PostPage';
 import PostList from './components/PostList';
-
 import CategoryPage from './components/CategoryPage';
 import AddPostForm from './components/AddPostForm';
 import ForgotPassword from './pages/ForgotPassword'; 
@@ -19,42 +17,65 @@ import VerifyCertificate from './components/VerifyCertificate';
 import Category from './pages/Category';
 import SignInSignUp from './components/SignInSignUp';
 import Footer from './components/Footer';
+import Notifications from './components/Notifications';
+import Policy from './components/Policy';
+import { loadUser } from './actions/authActions'; 
+
 const ScrollToTop = () => {
     const { pathname } = useLocation();
   
     useEffect(() => {
-      window.scrollTo(0, 0); // Scroll to the top of the page when the route changes.
+      window.scrollTo(0, 0); 
     }, [pathname]);
   
-    return null; // This component doesn't render anything.
-  };
+    return null; 
+};
+
+const PrivateRoute = ({ children }) => {
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    const policyAccepted = useSelector(state => state.auth.policyAccepted);
+
+    if (!isAuthenticated) {
+        return <SignInSignUp />;
+    }
+
+    if (!policyAccepted) {
+        return <Policy />;
+    }
+
+    return children;
+};
+
 const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(loadUser());
+    }, [dispatch]);
+
     return (
         <Provider store={store}>
             <Router>
-                <Layout> 
-                    
-                <ScrollToTop />
-
+                <Layout>
+                    <ScrollToTop />
                     <Routes>
                         <Route exact path="/" element={<Home />} />
-                        <Route path = "/category" element = {<Category/>}   />                   
-                          <Route exact path="/login" element={<SignInSignUp />} />
+                        <Route path="/category" element={<Category />} />
+                        <Route exact path="/login" element={<SignInSignUp />} />
                         <Route exact path="/register" element={<Register />} />
                         <Route exact path="/dashboard" element={<Dashboard />} />
-                        <Route path="/add-post" element={<AddPostForm/>} />
+                        <Route path="/add-post" element={<PrivateRoute><AddPostForm /></PrivateRoute>} />
                         <Route exact path="/admin-dashboard" element={<AdminDashboard />} />
-                                                <Route exact path="/explore" element={<PostList />} />
-
-                        <Route path="/post/:slug" element={<PostPage/>} />
-                        <Route path="/category/:category" element={<CategoryPage/>} />
+                        <Route path="/post/:slug" element={<PostPage />} />
+                        <Route path="/category/:category" element={<CategoryPage />} />
                         <Route exact path="/forgot-password" element={<ForgotPassword />} />
                         <Route path="/reset-password/:token" element={<ResetPassword />} />
                         <Route exact path="/verify/:uniqueId" element={<VerifyCertificate />} />
+                        <Route exact path="/posts-notification" element={<Notifications />} />
+                        <Route exact path="/explore" element={<PostList />} />
                         <Route exact path="/certificate-verification" element={<VerifyCertificate />} />
                     </Routes>
-                    <Footer/>
-
+                    <Footer />
                 </Layout>
             </Router>
         </Provider>
@@ -62,4 +83,3 @@ const App = () => {
 };
 
 export default App;
-
