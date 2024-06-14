c7a110b34de505cc41f290eb953b86e31b99cca6
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-06-15 00:31:35 +0530

    Update ForgotPassword.jsx

--- frontend/src/components/ForgotPassword.jsx
+++ frontend/src/components/ForgotPassword.jsx
@@ -8,7 +8,7 @@ const ForgotPassword = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axios.post('https://hogwarts-api-31may.onrender.com/forgot-password', { email });
+            await axios.post('https://hogwarts-api-14jun.onrender.com/forgot-password', { email });
             setMessage('Password reset email sent');
         } catch (error) {
             console.error(error);
