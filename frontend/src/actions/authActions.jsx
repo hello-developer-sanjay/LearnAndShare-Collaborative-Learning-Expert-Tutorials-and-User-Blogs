bc83d9ef9bc7f55e008e0b1fc840006cdbcf9b41
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-06-26 10:15:31 +0530

    Update authActions.jsx

--- frontend/src/actions/authActions.jsx
+++ frontend/src/actions/authActions.jsx
@@ -9,7 +9,7 @@ export const loadUser = () => async dispatch => {
         dispatch({ type: AUTHENTICATE_USER, payload: true });
     }
     try {
-        const res = await axios.get('https://hogwartsedx-api-15jun.onrender.com/api/auth/user');
+        const res = await axios.get('https://hogwartsedx-api-26jun.onrender.com/api/auth/user');
         const user = res.data.user; // Ensure user object is correctly extracted
 
         localStorage.setItem('user', JSON.stringify(res.data.user));
@@ -32,7 +32,7 @@ export const login = (email, password) => async dispatch => {
     const body = JSON.stringify({ email, password });
 
     try {
-        const res = await axios.post('https://hogwartsedx-api-15jun.onrender.com/api/auth/login', body, config);
+        const res = await axios.post('https://hogwartsedx-api-26jun.onrender.com/api/auth/login', body, config);
         const { token, user } = res.data;
         console.log('Login response:', res.data);
 
@@ -63,7 +63,7 @@ export const register = formData => async dispatch => {
     };
 
     try {
-        const res = await axios.post('https://hogwartsedx-api-15jun.onrender.com/api/auth/register', formData, config);
+        const res = await axios.post('https://hogwartsedx-api-26jun.onrender.com/api/auth/register', formData, config);
         const { token, user } = res.data;
         console.log('Register response:', res.data);
 
@@ -81,7 +81,7 @@ export const register = formData => async dispatch => {
 };
 export const acceptPolicy = () => async dispatch => {
     try {
-        const res = await axios.post('https://hogwartsedx-api-15jun.onrender.com/api/auth/accept-policy');
+        const res = await axios.post('https://hogwartsedx-api-26jun.onrender.com/api/auth/accept-policy');
         const { user } = res.data;
         console.log('Policy acceptance response:', res.data);
 
