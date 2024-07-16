5dd8f1dc17228a381414c77c8bdd8de0b12107d5
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-07-16 18:17:08 +0530

    Update README.m

--- README.m
+++ README.m
@@ -4,31 +4,31 @@ Welcome to **HogwartsEdX**, a magical platform where technology and learning con
 
 ## Homepage
 
-The journey begins at our Homepage: [https://hogwartsedx.vercel.app/](https://hogwartsedx.vercel.app/), where a simple click on the "Get Started" button will transport you to the PostPage: [https://hogwartsedx.vercel.app/explore](https://hogwartsedx.vercel.app/explore). Here, you can explore a myriad of blog titles, each promising a wealth of knowledge. Utilize the search and navigation functionalities to delve into topics that intrigue you.
+The journey begins at our [Homepage](https://hogwartsedx.vercel.app/), where a simple click on the **"Get Started"** button will transport you to the [PostPage](https://hogwartsedx.vercel.app/explore). Here, you can explore a myriad of blog titles, each promising a wealth of knowledge. Utilize the search and navigation functionalities to delve into topics that intrigue you.
 
 ## PostPage
 
-On the PostPage: [https://hogwartsedx.vercel.app/explore](https://hogwartsedx.vercel.app/explore), discover a collection of informative blog titles. Click on any title to navigate to the detailed blog post. The search functionality allows you to find specific topics of interest quickly and efficiently.
+On the [PostPage](https://hogwartsedx.vercel.app/explore), discover a collection of informative blog titles. Click on any title to navigate to the detailed blog post. The search functionality allows you to find specific topics of interest quickly and efficiently.
 
 ## Category Page
 
-Our Category Page: [https://hogwartsedx.vercel.app/category](https://hogwartsedx.vercel.app/category) features 20 diverse course categories. Follow your favorite categories to stay updated. When you follow a category, you'll receive timely notifications about updates directly to your registered email. A message, "You have successfully followed the category! Now you will receive timely notifications about updates in this category directly to your registered email," confirms your subscription.
+Our [Category Page](https://hogwartsedx.vercel.app/category) features 20 diverse course categories. Follow your favorite categories to stay updated. When you follow a category, you'll receive timely notifications about updates directly to your registered email. A message, **"You have successfully followed the category! Now you will receive timely notifications about updates in this category directly to your registered email,"** confirms your subscription.
 
 ## Add-Post Page
 
-Become a contributor on the Add-Post Page: [https://hogwartsedx.vercel.app/add-post](https://hogwartsedx.vercel.app/add-post). Fill out the form to add your blog post, but remember to accept our privacy policy first. Users must be logged in to submit a blog post, ensuring the safety and authenticity of our content.
+Become a contributor on the [Add-Post Page](https://hogwartsedx.vercel.app/add-post). Fill out the form to add your blog post, but remember to accept our privacy policy first. Users must be logged in to submit a blog post, ensuring the safety and authenticity of our content.
 
 ## BlogPost Completion Certificates
 
-Dedication pays off at HogwartsEdX! If you mark all posts within a particular category as completed, you'll be awarded a completion certificate from HogwartsEdX, signed by our founder, Sanjay Patidar. Each certificate is unique, featuring a unique ID that allows for verification.
+Dedication pays off at HogwartsEdX! If you mark all posts within a particular category as completed, you'll be awarded a completion certificate from HogwartsEdX, signed by our founder, **Sanjay Patidar**. Each certificate is unique, featuring a unique ID that allows for verification.
 
 ## Verify Certificate
 
-Verify your certificates on our Certificate Verification Page: [https://hogwartsedx.vercel.app/certificate-verification](https://hogwartsedx.vercel.app/certificate-verification). Enter the username, date of completion, or unique ID to display certificate details and download the certificate.
+Verify your certificates on our [Certificate Verification Page](https://hogwartsedx.vercel.app/certificate-verification). Enter the username, date of completion, or unique ID to display certificate details and download the certificate.
 
 ## Code Editor
 
-Unleash your coding prowess with our Code Editor: [https://hogwartsedx.vercel.app/editor](https://hogwartsedx.vercel.app/editor). This feature-rich tool allows users to add multiple windows, write code, and enjoy functionalities such as autosave, multiple themes, syntax highlighting, and download options (as text or JPEG). Your code remains intact even if you close the tab, thanks to our autosave functionality.
+Unleash your coding prowess with our [Code Editor](https://hogwartsedx.vercel.app/editor). This feature-rich tool allows users to add multiple windows, write code, and enjoy functionalities such as autosave, multiple themes, syntax highlighting, and download options (as text or JPEG). Your code remains intact even if you close the tab, thanks to our autosave functionality.
 
 ## Contributing
 
@@ -40,7 +40,7 @@ This project is licensed under the MIT License - see the LICENSE file for detail
 
 ## Contact
 
-For any inquiries or support, feel free to reach out to us at support@hogwartsedx.com.
+For any inquiries or support, feel free to reach out to us at **support@hogwartsedx.com**.
 
 ---
 
