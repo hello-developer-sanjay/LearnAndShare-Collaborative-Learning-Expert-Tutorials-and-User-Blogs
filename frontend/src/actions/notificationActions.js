d9f2d64e7f74a8935a30773a7c054de28b4462e3
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-07-16 17:48:06 +0530

    Update notificationActions.js

--- frontend/src/actions/notificationActions.js
+++ frontend/src/actions/notificationActions.js
@@ -27,7 +27,7 @@ export const fetchNotifications = () => async dispatch => {
     setAuthToken(token);
 
     try {
-        const response = await axios.get('https://hogwartsedx-api-26jun.onrender.com/api/categories/notifications', {
+        const response = await axios.get('https://hogwartsedx-api-16july.onrender.com/api/categories/notifications', {
             headers: {
                 'x-auth-token': token
             }
@@ -41,7 +41,7 @@ export const fetchNotifications = () => async dispatch => {
 export const addNotification = (notification) => async dispatch => {
     console.log('Adding notification:', notification);
     try {
-        const response = await axios.post('https://hogwartsedx-api-26jun.onrender.com/api/categories/notifications', notification);
+        const response = await axios.post('https://hogwartsedx-api-16july.onrender.com/api/categories/notifications', notification);
         dispatch({ type: ADD_NOTIFICATION_SUCCESS, payload: response.data });
     } catch (error) {
     }
@@ -58,7 +58,7 @@ export const markNotificationAsRead = (id, token) => async dispatch => {
         
 
         const response = await axios.put(
-            `https://hogwartsedx-api-26jun.onrender.com/api/notifications/${id}/read`,
+            `https://hogwartsedx-api-16july.onrender.com/api/notifications/${id}/read`,
             {},  // Ensure the body is correctly set
             {
                 headers: {
@@ -91,7 +91,7 @@ export const markNotificationAsRead = (id, token) => async dispatch => {
 export const followCategory = (category, token) => async dispatch => {
     try {
         const response = await axios.post(
-            'https://hogwartsedx-api-26jun.onrender.com/api/categories/follow-category',
+            'https://hogwartsedx-api-16july.onrender.com/api/categories/follow-category',
             { category },
             {
                 headers: {
@@ -99,7 +99,7 @@ export const followCategory = (category, token) => async dispatch => {
                 }
             }
         );
-        const followedCategories = response.data;  // Ensure this returns the updated list
+        const followedCategories = response.data;  
         updateLocalStorage(followedCategories);
         dispatch({ type: FOLLOW_CATEGORY_SUCCESS, payload: followedCategories });
         toast.success('You have successfully followed the category! Now you will receive timely notifications about updates in this category directly to your registered email.', {
@@ -127,7 +127,7 @@ export const followCategory = (category, token) => async dispatch => {
 export const unfollowCategory = (category, token) => async dispatch => {
     try {
         const response = await axios.post(
-            'https://hogwartsedx-api-26jun.onrender.com/api/categories/unfollow-category',
+            'https://hogwartsedx-api-16july.onrender.com/api/categories/unfollow-category',
             { category },
             {
                 headers: {
@@ -167,7 +167,7 @@ export const fetchFollowedCategories = () => async (dispatch) => {
     setAuthToken(token);
 
     try {
-        const response = await axios.get('https://hogwartsedx-api-26jun.onrender.com/api/categories/followed', {
+        const response = await axios.get('https://hogwartsedx-api-16july.onrender.com/api/categories/followed', {
             headers: {
                 'x-auth-token': token,
             },
