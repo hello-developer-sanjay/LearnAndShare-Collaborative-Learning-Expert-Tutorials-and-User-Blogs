dc40d8c17d6aaa232f3132fe2cc91d33ae74bbf9
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-08-07 17:51:38 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import styled, { keyframes } from 'styled-components';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
+ import { Helmet } from 'react-helmet';
 const fadeIn = keyframes`
   from {
     opacity: 0;
@@ -251,6 +251,234 @@ const Home = () => {
 
   return (
     <HomeContainer>
+      <Helmet>
+    
+    <title>LearnAndShare | Collaborative Learning, Expert Tutorials, and User Blogs</title>
+<meta
+ name="description"
+ content="
+LearnAndShare is an innovative educational platform merging engaging content with cutting-edge learning experiences. At LearnAndShare, users can create curated category modules and posts tailored to their interests and learning goals. With a focus on personalized learning, users can mark completed posts within specific categories, earning certificates that validate their mastery in various subjects. These certificates can be easily verified through the dedicated 'certificate-verification' page, ensuring credibility and recognition of user achievements.
+
+Furthermore, LearnAndShare offers a unique feature where users can choose to follow or unfollow specific categories, allowing for a customized learning journey. By following a category, users receive timely notifications via their registered email whenever new posts are added, empowering them to stay updated on the latest educational discoveries and content. With LearnAndShare, the possibilities for learning are endless, combining comprehensive educational resources with the transformative power of technology for an unparalleled learning experience."
+/>
+
+<link rel="icon" type="image/svg+xml" href="src/assets/learnandshare-logo.png" />
+
+<meta property="og:title" content="LearnAndShare | Collaborative Learning, Expert Tutorials, and User Blogs" />
+<meta property="og:description" content="LearnAndShare is an innovative educational platform merging engaging content with cutting-edge learning experiences. At LearnAndShare, users can create curated category modules and posts tailored to their interests and learning goals. With a focus on personalized learning, users can mark completed posts within specific categories, earning certificates that validate their mastery in various subjects. These certificates can be easily verified through the dedicated 'certificate-verification' page, ensuring credibility and recognition of user achievements.
+
+Furthermore, LearnAndShare offers a unique feature where users can choose to follow or unfollow specific categories, allowing for a customized learning journey. By following a category, users receive timely notifications via their registered email whenever new posts are added, empowering them to stay updated on the latest educational discoveries and content. With LearnAndShare, the possibilities for learning are endless, combining comprehensive educational resources with the transformative power of technology for an unparalleled learning experience."/> 
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://learnandshare.vercel.app/" />
+<meta property="og:image" content="https://sanjaybasket.s3.ap-south-1.amazonaws.com/learnandshare-logo.png" />
+<meta property="og:image:alt" content="LearningLog" />
+<meta property="og:site_name" content="LearnAndShare | Collaborative Learning, Expert Tutorials, and User Blogs" />
+<link rel="canonical" href="https://learnandshare.vercel.app/" />
+
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="LearnAndShare | Collaborative Learning, Expert Tutorials, and User Blogs" />
+<meta name="twitter:description" content="
+LearnAndShare is an innovative educational platform merging engaging content with cutting-edge learning experiences. At LearnAndShare, users can create curated category modules and posts tailored to their interests and learning goals. With a focus on personalized learning, users can mark completed posts within specific categories, earning certificates that validate their mastery in various subjects. These certificates can be easily verified through the dedicated 'certificate-verification' page, ensuring credibility and recognition of user achievements.
+
+Furthermore, LearnAndShare offers a unique feature where users can choose to follow or unfollow specific categories, allowing for a customized learning journey. By following a category, users receive timely notifications via their registered email whenever new posts are added, empowering them to stay updated on the latest educational discoveries and content. With LearnAndShare, the possibilities for learning are endless, combining comprehensive educational resources with the transformative power of technology for an unparalleled learning experience." />
+<meta name="twitter:image" content="https://sanjaybasket.s3.ap-south-1.amazonaws.com/learnandshare-logo.png" />
+
+
+<meta name="twitter:site" content="@LearnAndShare" />
+<meta name="twitter:creator" content="@LearnAndShare" />
+
+<meta name="keywords" content="educational blogs, tutorials, informative articles, personalized learning, curated modules, certificate verification, category notifications, learning journey, educational content, expert-written articles, how-to guides, academic resources, user-generated content, learning platform, online education, study resources, learning insights, educational discoveries, engaging content, educational achievements" />
+<meta name="author" content="Sanjay Patidar" />        <script type="application/ld+json">
+     {JSON.stringify({
+       '@context': 'http://schema.org',
+       '@type': 'Person',
+       "name": "Sanjay Patidar",
+       "birthDate": "1998-07-01",
+       "birthPlace": {
+         "@type": "Place",
+         "address": {
+           "@type": "PostalAddress",
+           "addressLocality": "Indore"
+         }
+       },
+       "alumniOf": {
+         "@type": "CollegeOrUniversity",
+         "name": "Chandigarh University",
+         "location": {
+           "@type": "Place",
+           "address": {
+             "@type": "PostalAddress",
+             "addressLocality": "Chandigarh",
+             "addressRegion": "Punjab",
+             "addressCountry": "India"
+           }
+         }
+       },
+       "address": [
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Indore",
+           "addressRegion": "Madhya Pradesh",
+           "postalCode": "452001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Chandigarh",
+           "addressRegion": "Punjab",
+           "postalCode": "160001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Mumbai",
+           "addressRegion": "Maharashtra",
+           "postalCode": "400001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Bangalore",
+           "addressRegion": "Karnataka",
+           "postalCode": "560001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Delhi",
+           "addressRegion": "Delhi",
+           "postalCode": "110001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Kolkata",
+           "addressRegion": "West Bengal",
+           "postalCode": "700001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Chennai",
+           "addressRegion": "Tamil Nadu",
+           "postalCode": "600001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Hyderabad",
+           "addressRegion": "Telangana",
+           "postalCode": "500001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Pune",
+           "addressRegion": "Maharashtra",
+           "postalCode": "411001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Ahmedabad",
+           "addressRegion": "Gujarat",
+           "postalCode": "380001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Jaipur",
+           "addressRegion": "Rajasthan",
+           "postalCode": "302001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Lucknow",
+           "addressRegion": "Uttar Pradesh",
+           "postalCode": "226001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Bhopal",
+           "addressRegion": "Madhya Pradesh",
+           "postalCode": "462001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Nagpur",
+           "addressRegion": "Maharashtra",
+           "postalCode": "440001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Visakhapatnam",
+           "addressRegion": "Andhra Pradesh",
+           "postalCode": "530001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Kochi",
+           "addressRegion": "Kerala",
+           "postalCode": "682001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Guwahati",
+           "addressRegion": "Assam",
+           "postalCode": "781001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Bhubaneswar",
+           "addressRegion": "Odisha",
+           "postalCode": "751001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Dehradun",
+           "addressRegion": "Uttarakhand",
+           "postalCode": "248001",
+           "addressCountry": "India"
+         },
+         {
+           "@type": "PostalAddress",
+           "addressLocality": "Raipur",
+           "addressRegion": "Chhattisgarh",
+           "postalCode": "492001",
+           "addressCountry": "India"
+         }
+       ],
+       "worksFor": {
+         "@type": "Organization",
+         "name": "LearnAndShare" 
+       },
+       "url": "https://learnandshare.vercel.app/",
+       "sameAs": [
+         "https://www.linkedin.com/in/sanjay-patidar-25b580292/",
+         "https://github.com/hello-developer-sanjay",
+         "https://www.instagram.com/sanjay_patidar_mcmxcviii/",
+         "https://learnandshare.vercel.app/",
+                    "https://learnandshare.vercel.app/certificate-verification",
+         
+
+
+
+       ]
+ 
+
+     })}
+   </script>
+
+
+  </Helmet>	
       <Title>LearnAndShare</Title>
       <Subtitle>Where Technology Powers Next-Level Learning</Subtitle>
       <CarouselContainer>
